84893c75c296d9d1403ad5a6c31c296d11af2a96
Rare Raptor <connorwtuncc@gmail.com>
2022-06-13 17:02:43 -0400

    started questions and added necessary dependencies

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const util = require('util');
 
 // TODO: Create an array of questions for user input
-const questions = [''];
+const questions = [
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the title of your project?',
+                name: 'project-title',
+            },
+            {
+                type: 'input',
+                message: 'Write a brief description of your project',
+                name: 'description'
+            }
+        ])
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
